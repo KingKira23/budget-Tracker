1e3dc789828de5e24142123ff4e71d52bdea6e74
Alberto Ruiz <alberto.ruiz.iii@gmail.com>
2020-03-27 20:09:56 -0500

    added the service worker

---
+++ public/service-worker.js
@@ -0,0 +1,50 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/favicon.ico",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", (evt) => {
+    evt.waitUntil(
+        cache.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully")
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    )
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (evt) => {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key)
+                    }
+                })
+            )
+        })
+    )
+    self.ClientRectList.claim()
+});
+
+self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+          caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+              return response || fetch(evt.request);
+            });
+          })
+        );
+    }
+    return Response
+})
